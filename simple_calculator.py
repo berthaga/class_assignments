63a3506e2a66215c3c9ada0092b6599ef34360ee
berthaga <bgarna@hotmail.com>
2017-06-02 15:54:51 +0000

    simple calculator example

---
+++ simple_calculator.py
@@ -0,0 +1,6 @@
+number1=input('enter the first number: ')
+number2=input('enter the second number: ')
+print'The sum is ........',number1+number2
+print'The difference is..',number1-number2
+print'The quotient is....',number1/number2
+print'The product is.....',number1*number2
