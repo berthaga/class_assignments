16f0f1ac9937846c9f3b59a1b06e52ba5f8ec6ab
berthaga <bgarna@hotmail.com>
2017-06-07 11:15:35 +0000

    whir_loops_example

---
+++ while_loops_examples.py
@@ -0,0 +1,31 @@
+
+#Break
+var=4
+while var<=60:
+	var=var+4
+	if var==28:
+		break
+		print var,"you are on course"
+	print var
+	    
+
+#Continue
+var=4
+while var<=60:
+	var=var+4
+	if var==28:
+		continue
+		print var,"you are on course"
+	print var
+
+# Pass
+var=4
+while var<=60:
+	var=var+4
+	if var==28:
+		pass
+		print var,"you are on course"
+	else:
+		print var
+    
+	
