3be88c0a6001d722c5ce8c117ac10d1ffc35e579
berthaga <bgarna@hotmail.com>
2017-06-10 15:30:51 +0000

    loops_and_nested_loops.py

---
+++ loops_and_nested_loops.py
@@ -0,0 +1,28 @@
+for x in range(0,100):
+	print x
+
+i = 2
+while(i<10):
+   j = 2
+   while(j <= (i/j)):
+      if not(i%j):
+       break
+      j = j + 1
+      print j,'j next number'
+   if (j > i/j) : print i, " is prime"
+   i = i + 1
+   print i,'hi, i next number'
+
+print "Good bye!"
+
+
+a=()
+if not a:
+# check if a is empty, that is there is nothing in a container. 
+# if a is empty then it should print list is empty, otherwise 
+# it sholud print list is not empty
+  print("List is empty")
+else:
+	print("List is not empty")
+
+
