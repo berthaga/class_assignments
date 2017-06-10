e50a110f24d6d480e417010a4564cfede19ddcec
berthaga <bgarna@hotmail.com>
2017-06-10 15:29:16 +0000

    function.py

---
+++ function.py
@@ -0,0 +1,25 @@
+def form():
+	name=raw_input('what is your name? ')
+	age=raw_input('How old are you? ')
+	gender=raw_input('M/F')
+	address=raw_input('where do you stay? ')
+	return name,age,gender,address
+print form()
+
+
+def addition():
+	print 2+2
+addition()
+
+
+def addition(first,second):
+	print first+second
+	pass
+addition(8,9)
+
+def addition(first,9):
+	print first+second
+	pass
+addition(8,9)
+
+
